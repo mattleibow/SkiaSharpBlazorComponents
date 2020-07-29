7a8911e79ae85e8b6c3cd93c323c7f7852ce98e3
Matthew Leibowitz <mattleibow@live.com>
2020-07-30 00:26:25 +0200

    Some code

---
+++ SkiaSharpBlazorComponents/SkiaSharpImages/SkiaSharpImage.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using SkiaSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace SkiaSharpBlazorComponents.SkiaSharpImages
+{
+	public partial class SkiaSharpImage : ComponentBase
+	{
+		public string ImageUrl { get; private set; }
+
+		[Parameter]
+		public EventCallback<PaintImageEventArgs> OnPaintImage { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+
+			using var surface = SKSurface.Create(new SKImageInfo(100, 100));
+
+			await OnPaintImage.InvokeAsync(new PaintImageEventArgs(surface));
+
+			using var image = surface.Snapshot();
+			var guid = SkiaSharpImagesController.Save(image);
+
+			ImageUrl = "/api/skiasharpimages?g=" + guid;
+		}
+	}
+
+	public class PaintImageEventArgs : EventArgs
+	{
+		public PaintImageEventArgs(SKSurface surface)
+		{
+			Surface = surface;
+		}
+
+		public SKSurface Surface { get; }
+	}
+}
---
+++ SkiaSharpBlazorComponents/SkiaSharpImages/SkiaSharpImagesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkiaSharp;
+using System;
+using System.IO;
+
+namespace SkiaSharpBlazorComponents
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SkiaSharpImagesController : ControllerBase
+	{
+		private static readonly string rootPath;
+
+		static SkiaSharpImagesController()
+		{
+			rootPath = Path.Combine(Path.GetTempPath(), "SkiaSharpImages");
+			if (!Directory.Exists(rootPath))
+				Directory.CreateDirectory(rootPath);
+		}
+
+		[HttpGet]
+		public ActionResult Get(string g)
+		{
+			if (!Guid.TryParse(g, out _))
+				return NotFound();
+
+			var path = Path.Combine(rootPath, g);
+
+			if (!System.IO.File.Exists(path))
+				return NotFound();
+
+			var file = System.IO.File.OpenRead(path);
+			return File(file, "image/png");
+		}
+
+		public static string Save(SKImage image)
+		{
+			var guid = Guid.NewGuid().ToString();
+
+			using (var file = System.IO.File.Create(Path.Combine(rootPath, guid)))
+			{
+				using var pixmap = image.PeekPixels();
+				pixmap.Encode(file, SKPngEncoderOptions.Default);
+			}
+
+			return guid;
+		}
+	}
+}
